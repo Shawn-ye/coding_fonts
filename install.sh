7af099cf9c7a8f115a5890512c3a20243f02b364
Shawn Ye <shawnye90@163.com>
2017-02-18 10:50:29 +0800

    modify install.sh: auto detect folder

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
 sudo mkdir -p /usr/share/fonts/custom_font
-sudo cp /home/ye/coding_fonts/*.ttf /usr/share/fonts/custom_font
-
+sudo cp $SHELL_FOLER/*.ttf /usr/share/fonts/custom_font
+# sudo cp /home/ye/coding_fonts/*.ttf /usr/share/fonts/custom_font
 sudo chmod 644 /usr/share/fonts/custom_font/*.ttf
-
 cd /usr/share/fonts/custom_font
 sudo mkfontscale
 sudo mkfontdir

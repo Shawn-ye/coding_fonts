13f3a8b44ebe525b84001b6cc0a669fc12dcb19f
shawn <shawnye@163.com>
2017-12-04 21:06:08 +0800

    modify install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 sudo mkdir -p /usr/share/fonts/custom_font
-sudo cp $SHELL_FOLER/*.ttf /usr/share/fonts/custom_font
+sudo cp $PWD/*.ttf /usr/share/fonts/custom_font
 # sudo cp /home/ye/coding_fonts/*.ttf /usr/share/fonts/custom_font
 sudo chmod 644 /usr/share/fonts/custom_font/*.ttf
 cd /usr/share/fonts/custom_font
